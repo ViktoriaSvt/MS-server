9c0be114198f72439f66e1afa0a114926a02d716
Viktoria Stoycheva <vikivikisto@abv.bg>
2025-03-13 22:07:55 +0200

    setup microservices as independent modules 2

--- common-utils/src/main/java/com/skytales/common/common_utils/common/security/SessionResponse.java
+++
@@ -1,4 +0,0 @@
-package com.skytales.common.common_utils.common.security;
-
-public record SessionResponse(String email, String username, String id, String role, String cartId) { }
-
--- common-utils/src/main/java/com/skytales/common/common_utils/common/state_engine/model/UpdateType.java
+++
@@ -1,5 +0,0 @@
-package com.skytales.common.common_utils.common.state_engine.model;
-
-public enum UpdateType {
-    NEW_BOOK, REMOVE_BOOK, STOCK_CHANGE
-}
