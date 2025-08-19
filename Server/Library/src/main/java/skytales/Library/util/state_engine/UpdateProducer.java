f4f7b0f693006ea9700dcf9a391d508885c48e58
Viktoria Stoycheva <vikivikisto@abv.bg>
2025-08-19 16:47:58 +0300

    refactor: update Kafka message conversion to use ModelMapper instead of reflection

--- Server/Library/src/main/java/skytales/Library/util/state_engine/UpdateProducer.java
+++ Server/Library/src/main/java/skytales/Library/util/state_engine/UpdateProducer.java
@@ -1,14 +1,13 @@
 package skytales.Library.util.state_engine;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
 import skytales.Library.util.state_engine.dto.BookMessage;
 import skytales.Library.util.state_engine.model.UpdateType;
 import skytales.Library.util.state_engine.utils.KafkaMessage;
 
-import java.lang.reflect.Field;
-import java.math.BigDecimal;
-import java.util.UUID;
 
 @Service
 public class UpdateProducer {
@@ -21,53 +20,24 @@ public class UpdateProducer {
     }
 
     public void sendBookUpdate(UpdateType updateType, Object book) {
+        BookMessage bookMessage = createBookMessageFromBook(book);
+
+        KafkaMessage<BookMessage> request = new KafkaMessage<>(bookMessage);
+        request.setType(updateType.toString());
+
+        kafkaTemplate.send("book-updates", request);
+
+    }
+
+    private BookMessage createBookMessageFromBook(Object book) {
+        ModelMapper modelMapper = new ModelMapper();
         try {
-            BookMessage bookMessage = createBookMessageFromBook(book);
-
-            KafkaMessage<BookMessage> request = new KafkaMessage<>(bookMessage);
-            request.setType(updateType.toString());
-
-            kafkaTemplate.send("book-updates", request);
-
-        } catch (NoSuchFieldException | IllegalAccessException e) {
+            return modelMapper.map(book, BookMessage.class);
+        } catch (Exception e) {
             e.printStackTrace();
+            throw new RuntimeException("Error mapping Book to BookMessage: " + e.getMessage(), e);
         }
     }
 
-    private BookMessage createBookMessageFromBook(Object book) throws NoSuchFieldException, IllegalAccessException {
-        Field idField = book.getClass().getDeclaredField("id");
-        idField.setAccessible(true);
-        UUID id = (UUID) idField.get(book);
-
-        Field titleField = book.getClass().getDeclaredField("title");
-        titleField.setAccessible(true);
-        String title = (String) titleField.get(book);
-
-        Field genreField = book.getClass().getDeclaredField("genre");
-        genreField.setAccessible(true);
-        String genre = (String) genreField.get(book);
-
-        Field authorField = book.getClass().getDeclaredField("author");
-        authorField.setAccessible(true);
-        String author = (String) authorField.get(book);
-
-        Field coverImageUrlField = book.getClass().getDeclaredField("coverImageUrl");
-        coverImageUrlField.setAccessible(true);
-        String coverImageUrl = (String) coverImageUrlField.get(book);
-
-        Field yearField = book.getClass().getDeclaredField("year");
-        yearField.setAccessible(true);
-        int year = (int) yearField.get(book);
-
-        Field priceField = book.getClass().getDeclaredField("price");
-        priceField.setAccessible(true);
-        BigDecimal price = (BigDecimal) priceField.get(book);
-
-        Field quantityField = book.getClass().getDeclaredField("quantity");
-        quantityField.setAccessible(true);
-        int quantity = (int) quantityField.get(book);
-
-        return new BookMessage(id, title, genre, author, coverImageUrl, year, price, quantity);
-    }
 
 }
